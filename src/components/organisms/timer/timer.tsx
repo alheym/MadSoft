f79166532423e1be070b11ce40d482e1d23da41a
alheym <alheym.mw@gmail.com>
2024-06-13 18:55:58 +0300

    improv: timer

--- src/components/organisms/timer/timer.tsx
+++ src/components/organisms/timer/timer.tsx
@@ -8,26 +8,39 @@ import type { TimerProps } from './types'
 export const Timer: FC<TimerProps> = ({ initialTime, onExpire }) => {
   const [time, setTime] = useState(() => {
     const storedTestTime = localStorage.getItem('testTime')
-    return storedTestTime ? parseInt(storedTestTime) : initialTime
+    const storedStartTime = localStorage.getItem('startTime')
+    const startTime = storedStartTime ? parseInt(storedStartTime) : Date.now()
+    const elapsedTime = Date.now() - startTime
+    const remainingTime = initialTime - elapsedTime
+
+    return storedTestTime ? parseInt(storedTestTime) : remainingTime
   })
+
   const timerRef = useRef<NodeJS.Timeout | null>(null)
 
   useEffect(() => {
+    const startTime = localStorage.getItem('startTime') ? parseInt(localStorage.getItem('startTime')!) : Date.now()
+    if (!localStorage.getItem('startTime')) {
+      localStorage.setItem('startTime', startTime.toString())
+    }
+
     timerRef.current = setInterval(() => {
-      setTime(prevTime => {
-        if (prevTime <= 1000) {
-          clearInterval(timerRef.current!)
-          onExpire()
-          return 0
-        }
-        return prevTime - 1000
-      })
+      const elapsedTime = Date.now() - startTime
+      const remainingTime = initialTime - elapsedTime
+
+      setTime(remainingTime)
+
+      if (remainingTime <= 0) {
+        clearInterval(timerRef.current!)
+        onExpire()
+        setTime(0)
+      }
     }, 1000)
 
     return () => {
       if (timerRef.current) clearInterval(timerRef.current)
     }
-  }, [])
+  }, [initialTime, onExpire])
 
   useEffect(() => {
     localStorage.setItem('testTime', time.toString())
